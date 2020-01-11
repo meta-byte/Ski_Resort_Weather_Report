0e18320571909b357fa7ea68a0d2f96db532295b
Garrett Howard <hylander.garrett@gmail.com>
2020-01-11 10:30:07 -0700

    updated twitter feed to show all tweets and present in a scrollbox

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,66 +1,79 @@
+var resorts = [
+    {
+        Resort: "Zermatt",
+        ID: 13026,
+        twitterURL: "https://twitter.com/zermatt_tourism"
+    }
+]
+function twitterMap(resort) {
+    var feed = $("<a>")
+    $(feed).addClass("twitter-timeline")
+    $(feed).attr("id", "timeline")
+    $(feed).attr("href", resort.twitterURL)
+
+    $("#twitter").append(feed)
+
+    // <a class="twitter-timeline" id="timeline" href="">
+    // </a>
+    console.log(resort.twitterURL)
+    console.log(feed)
+    $.getScript("https://platform.twitter.com/widgets.js")
+}
+function checkReports() {
+    var search = ""
+    var selected = $(".browser-default :selected").val()
+
+    for (i = 0; i < resorts.length; i++) {
+        if (resorts[i].Resort === selected) {
+            search = resorts[i].ID
+            console.log(resorts[i].ID)
+            twitterMap(resorts[i])
+        }
+    }
+
+    var forecastURL = "https://api.weatherunlocked.com/api/resortforecast/" + search + "?app_id=6461851d&app_key=4c6ecb1f135f755162b13545e275e1ff"
+    var snowURL = "https://api.weatherunlocked.com/api/snowreport/" + search + "?app_id=6461851d&app_key=4c6ecb1f135f755162b13545e275e1ff"
+
+    $.ajax({
+        url: forecastURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+        updateforecastCards(response)
+    })
+
+    $.ajax({
+        url: snowURL,
+        method: "GET",
+    }).then(function (response) {
+
+        console.log(response)
+        updatesnowCards(response)
+    })
+}
+
+function updateforecastCards(response) {
+    forecast = response.forecast[4]
+    $("#data5").text(forecast.base.wx_desc)
+    $("#data6").text(forecast.base.temp_f)
+    $("#data7").text(forecast.base.windspd_mph)
+    $("#weatherIcon").attr("src", "assets/images/PNG/" + forecast.base.wx_icon.replace(".gif", ".png"));
+    $("#data6").text(forecast.base.temp_f + " °F")
+    $("#data7").text(forecast.base.windspd_mph + " mph")
+
+}
+
+function updatesnowCards(response) {
+    $("#data2").text(response.lastsnow.substring(0, 5))
+    $("#lastSnow").text(response.lastsnow_in + " in")
+    $("#data3").text(response.lowersnow_in + " in")
+    $("#data4").text(response.uppersnow_in + " in")
+    $("#data1").text(response.conditions)
+
+}
+
+
 $(document).ready(function () {
-
-    resorts = [
-        {
-            Resort: "Zermatt",
-            ID: 13026,
-        }
-    ]
-
-
-    function checkReports() {
-        var search = ""
-        var selected = $(".browser-default :selected").val()
-
-        for (i = 0; i < resorts.length; i++) {
-            if (resorts[i].Resort === selected) {
-                search = resorts[i].ID
-                console.log(resorts[i].ID)
-            }
-        }
-
-        var forecastURL = "https://api.weatherunlocked.com/api/resortforecast/" + search + "?app_id=6461851d&app_key=4c6ecb1f135f755162b13545e275e1ff"
-        var snowURL = "https://api.weatherunlocked.com/api/snowreport/" + search + "?app_id=6461851d&app_key=4c6ecb1f135f755162b13545e275e1ff"
-
-        $.ajax({
-            url: forecastURL,
-            method: "GET",
-        }).then(function (response) {
-            console.log(response)
-            updateforecastCards(response)
-        })
-
-        $.ajax({
-            url: snowURL,
-            method: "GET",
-        }).then(function (response) {
-
-            console.log(response)
-            updatesnowCards(response)
-        })
-    }
-
-    function updateforecastCards(response) {
-
-
-        forecast = response.forecast[4]
-        $("#data5").text(forecast.base.wx_desc)
-        $("#data6").text(forecast.base.temp_f)
-        $("#data7").text(forecast.base.windspd_mph)
-        $("#weatherIcon").attr("src", "assets/images/PNG/" + forecast.base.wx_icon.replace(".gif", ".png"));
-        $("#data6").text(forecast.base.temp_f + " °F")
-        $("#data7").text(forecast.base.windspd_mph + " mph")
-
-    }
-
-    function updatesnowCards(response) {
-        $("#data2").text(response.lastsnow_in + " in")
-        $("#data3").text(response.lowersnow_in + " in")
-        $("#data4").text(response.uppersnow_in + " in")
-        $("#data1").text(response.conditions)
-
-    }
-
 
     $("#searchButton").on("click", function () {
         checkReports();
@@ -73,3 +86,8 @@ $(document).ready(function () {
     });
 
 })
+
+{/* <a class="twitter-timeline"
+  href="https://twitter.com/{screen_name}">
+Tweets by @{screen_name}
+</a> */}
